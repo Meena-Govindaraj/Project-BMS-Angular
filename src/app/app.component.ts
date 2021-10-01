ce0fe898ad02aeb646cf15e7e591b365d5556e44
Meena-Govindaraj <88269382+Meena-Govindaraj@users.noreply.github.com>
2021-10-01 15:24:20 +0530

    changed designs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,9 @@ export class AppComponent {
   constructor(public router:Router){}
   
   ngOnInit(): void {
+     //this.router.navigate(['employeeop',2])
+     //this.router.navigate(['account',25,"Savings"])
     this.router.navigate(['home'])
-    //this.router.navigate(['account',25,"Savings"])
-    // this.router.navigate(['adminop'])
     //this.router.navigate(['customeroperations',32])
     
   }
